f7ee2959f3b363e98bc36e884780f5c92a2ba516
takenokoya <takuya1002@gmail.com>
2020-05-14 10:47:01 +0900

    fix download.py for scraping and download animal_img

--- download.py
+++ download.py
@@ -15,16 +15,17 @@ key = os.environ.get('API_KEY')
 secret = os.environ.get('API_SECRET')
 wait_time = 1
 
+# コマンド実行の際の2番めの引数をanimal_nameに代入
+animal_name = sys.argv[1]
 # 保存フォルダ指定
-animalname = sys.argv[1]
-savedir = './' + animalname
+save_dir = './' + animal_name
 
 # flickrクライアント
 flickr = FlickrAPI(key, secret, format='parsed-json')
 
 # 検索を実行
 result = flickr.photos.search(
-    text=animalname,
+    text=animal_name,
     per_page=400,
     media='photos',
     sort='relevance',
@@ -33,4 +34,15 @@ result = flickr.photos.search(
 )
 
 photos = result['photos']
-pprint(photos)
+# pprint(photos)
+
+# APIからの取得結果を特定のファイルに保存する
+for i, photo in enumerate(photos['photo']):
+    url_q = photo['url_q']
+    file_path = save_dir + '/' + photo['id'] + '.jpg'
+    if os.path.exists(file_path): continue
+    # ネット上からファイルをダウンロードし保存するのにurlretrieveを使う
+    # arg: 目的のurl, 保存先のファイル名
+    urlretrieve(url_q, file_path)
+    # ファイルのダウンロード後に1秒の実行遅延をいれる
+    time.sleep(wait_time)
